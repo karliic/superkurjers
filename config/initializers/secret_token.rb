563e3ff55a3bd1d192be7777d8d77aec0411a11c
Karlis <karlis@karlis-VirtualBox.(none)>
2013-02-19 11:02:44 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Superkurjers::Application.config.secret_token = 'd5b6e7870dea60cdd38c70a82bbdd1aa61ab65c4680c2fb6d3300d210daeee77f7a215f526b8377ad5491482a2c8dd8c4a7d4a429e880aa447cef52028ea0df5'
